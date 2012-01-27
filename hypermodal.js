ceb0558eaba576d4635ac76ad6cbb598471e7f9a
Yuki KAN <re@pixely.jp>
2012-01-27 17:21:54 +0900

    test

---
+++ hypermodal.js
@@ -0,0 +1,24 @@
+/*!
+ * Hypermodal/dev for Prototype.js
+ *
+ * Copyright (c) 2012 Yuki KAN
+ * Licensed under the MIT-License.
+ *
+ * http://akkar.in/projects/hypermodal/
+**/
+var Hypermodal = Class.create({
+	/**
+	 * Constructor
+	**/
+	initialize: function _init(p) {
+		this.modalID     = p.modalID     || null;
+		this.modalClass  = p.modalClass  || 'hypermodal';
+		this.modalWidth  = p.modalWidth  || 'auto';
+		this.modalHeight = p.modalHeight || 'auto';
+		this.modalStyle  = p.modalStyle  || {};
+		this.onClose     = p.onClose     || null;
+		
+		this.disableCloseButton = p.disableCloseButton || false;
+		this.disableCloseByMask = p.disableCloseByMask || false;
+	}//<--initialize()
+});
